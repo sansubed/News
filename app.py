ca9344729958356a34e4950de5b1004d8c8cf90f
Santona Subedi <129696036+sansubed@users.noreply.github.com>
2025-12-14 19:39:19 -0600

    dataframe for the news

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Text Classification')
 
-st.text_area('Text to translate')
-st.button('Submit')
+news = st.text_area('Text to translate')
+if st.button('Submit'):
+    dict = {'news': [news]}
+    df = pd.DataFrame(dict)
+    st.dataframe(df)
+    st.write('News submitted.')
+
 
